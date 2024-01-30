6d6e4febe65f5754b11d3621b1a4927dd5c7b476
Anurag Sharda <145989927+AnuragSharda111@users.noreply.github.com>
2024-01-31 00:52:37 +0530

    Create sum.cpp
    commit

---
+++ sum.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  int a, b, c;
+  cout<<"Enter a: ";
+  cin>>a;
+  cout<<"Enter b: ";
+  cin>>b;
+
+  c= a+b;
+
+  cout<<"Sum of "<<a<<"and"<<b<<": "<<c<<endl;
+
+  return 0;
+}
